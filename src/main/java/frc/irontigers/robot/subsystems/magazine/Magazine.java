883560c0979f993bfc2d36ac2bf606c3ceba6583
Nick Manos <nmanos@eastonrobotics.org>
2022-02-20 23:37:21 -0500

    Add methods to Magazine for keeping track of balls

--- src/main/java/frc/irontigers/robot/subsystems/magazine/Magazine.java
+++ src/main/java/frc/irontigers/robot/subsystems/magazine/Magazine.java
@@ -4,18 +4,24 @@
 
 package frc.irontigers.robot.subsystems.magazine;
 
+import java.util.LinkedList;
+
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
+import edu.wpi.first.util.sendable.SendableRegistry;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.irontigers.robot.Constants.MagazineVals;
+import frc.irontigers.robot.subsystems.magazine.Ball.Position;
 
 public class Magazine extends SubsystemBase {
   /** Creates a new InFeed. */
 
   private WPI_TalonFX conveyor;
   private DigitalInput sensor;
+
+  private LinkedList<Ball> balls = new LinkedList<>();
   
   public Magazine() {
     conveyor = new WPI_TalonFX(MagazineVals.MOTOR_ID);
@@ -32,6 +38,56 @@ public class Magazine extends SubsystemBase {
 
   public boolean beamBroken() {
     return sensor.get();
+  }
+
+  public void addBall() {
+    Ball b = new Ball();
+    SendableRegistry.addChild(this, b);
+    balls.add(b);
+  }
+
+  public void removeFirstBall() {
+    if (!balls.isEmpty()) {
+      balls.removeFirst();
+    }
+  }
+
+  public void removeLastBall() {
+    if (!balls.isEmpty()) {
+      balls.removeLast();
+    }
+  }
+
+  public Ball getBallAt(Position position) {
+    for (Ball b : balls) {
+      if (b.getPosition() == position) {
+        return b;
+      }
+    }
+
+    return null;
+  }
+  
+  public Ball getBall(int index) {
+    return balls.get(index);
+  }
+
+  public void updateBallPositions() {
+    updateBallPositions(true);
+  }
+
+  public void updateBallPositions(boolean forward) {
+    for (Ball b : balls) {
+      if (forward) {
+        b.forward();
+      } else {
+        b.back();
+      }
+
+      if (b.getPosition() == null) {
+        balls.remove(b);
+      }
+    }
   }
 
   @Override
